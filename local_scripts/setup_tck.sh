fb4d5c07a7bc2560e6aa5f019815a8a6c7e82a92
olivier lamy <oliver.lamy@gmail.com>
2019-12-19 19:22:27 +1000

    backup some local script
    Signed-off-by: olivier lamy <oliver.lamy@gmail.com>

---
+++ local_scripts/setup_tck.sh
@@ -0,0 +1,27 @@
+# target/base/webapps/
+set -x
+cd /Users/olamy/dev/sources/open-sources/jetty/jetty.project/jetty-home/target
+rm -rf jetty-base
+mkdir jetty-base
+cd jetty-base
+mkdir etc
+#cd etc
+#touch keystore
+#cd ../
+#/Library/Java/JavaVirtualMachines/jdk-11.0.2.jdk/Contents/Home/bin/java -jar ../jetty-home/start.jar --approve-all-licenses --create-startd --add-to-start=resources,server,http,https,http2c,webapp,deploy,jsp,logging-log4j2
+/Library/Java/JavaVirtualMachines/jdk-11.0.2.jdk/Contents/Home/bin/java -jar ../jetty-home/start.jar --approve-all-licenses --create-startd --add-to-start=resources,server,http,http2c,webapp,deploy,jsp
+cd /Users/olamy/dev/sources/open-sources/jetty/tck_tests/servlet-tck
+find . -name "*.war" -exec cp {} /Users/olamy/dev/sources/open-sources/jetty/jetty.project/jetty-home/target/jetty-base/webapps \;
+cd /Users/olamy/dev/sources/open-sources/jetty/tck-run
+cp realm.ini /Users/olamy/dev/sources/open-sources/jetty/jetty.project/jetty-home/target/jetty-base/start.d/
+cp realm.properties /Users/olamy/dev/sources/open-sources/jetty/jetty.project/jetty-home/target/jetty-base/etc/
+cp test-realm.xml /Users/olamy/dev/sources/open-sources/jetty/jetty.project/jetty-home/target/jetty-base/etc/
+cp clientcert.jks /Users/olamy/dev/sources/open-sources/jetty/jetty.project/jetty-home/target/jetty-base/etc/
+cp cacerts.jks /Users/olamy/dev/sources/open-sources/jetty/jetty.project/jetty-home/target/jetty-base/etc/
+cp cacerts.jks /Users/olamy/dev/sources/open-sources/jetty/jetty.project/jetty-home/target/jetty-base/etc/keystore
+cp log4j2.xml  /Users/olamy/dev/sources/open-sources/jetty/jetty.project/jetty-home/target/jetty-base/resources
+cp http.ini /Users/olamy/dev/sources/open-sources/jetty/jetty.project/jetty-home/target/jetty-base/start.d/
+
+cd /Users/olamy/dev/sources/open-sources/jetty/jetty.project/jetty-home/target/jetty-base
+/Library/Java/JavaVirtualMachines/jdk-11.0.2.jdk/Contents/Home/bin/java -Duser.language=en -Duser.country=US -jar ../jetty-home/start.jar 
+#/Library/Java/JavaVirtualMachines/jdk-11.0.2.jdk/Contents/Home/bin/java    -Duser.language=en -Duser.country=US -Djavax.net.ssl.trustStore=etc/cacerts.jks -Djavax.net.ssl.keyStore=etc/clientcert.jks -Djavax.net.ssl.keyStorePassword=changeit -Dorg.eclipse.jetty.ssl.password=changeit -jar ../jetty-home/start.jar jetty.sslContext.trustStorePath=etc/cacerts.jks jetty.sslContext.keyStorePassword=changeit org.eclipse.jetty.ssl.password=changeit
---
+++ local_scripts/setup_tck_ws.sh
@@ -0,0 +1,21 @@
+set -x
+cd /Users/olamy/dev/sources/open-sources/jetty/jetty.project/jetty-home/target
+rm -rf jetty-base
+mkdir jetty-base
+cd jetty-base
+mkdir etc
+/Library/Java/JavaVirtualMachines/jdk-11.0.2.jdk/Contents/Home/bin/java -jar ../jetty-home/start.jar --approve-all-licenses --create-startd --add-to-start=resources,server,http,https,http2c,webapp,deploy,jsp,websocket,logging-log4j2
+cd /Users/olamy/dev/sources/open-sources/jetty/tck_tests/websocket-tck
+find . -name "*.war" -exec cp {} /Users/olamy/dev/sources/open-sources/jetty/jetty.project/jetty-home/target/jetty-base/webapps \;
+cd /Users/olamy/dev/sources/open-sources/jetty/tck-run
+cp realm.ini /Users/olamy/dev/sources/open-sources/jetty/jetty.project/jetty-home/target/jetty-base/start.d/
+cp realm.properties /Users/olamy/dev/sources/open-sources/jetty/jetty.project/jetty-home/target/jetty-base/etc/
+cp test-realm.xml /Users/olamy/dev/sources/open-sources/jetty/jetty.project/jetty-home/target/jetty-base/etc/
+cp clientcert.jks /Users/olamy/dev/sources/open-sources/jetty/jetty.project/jetty-home/target/jetty-base/etc/
+cp cacerts.jks /Users/olamy/dev/sources/open-sources/jetty/jetty.project/jetty-home/target/jetty-base/etc/
+cp cacerts.jks /Users/olamy/dev/sources/open-sources/jetty/jetty.project/jetty-home/target/jetty-base/etc/keystore
+cp log4j2.xml  /Users/olamy/dev/sources/open-sources/jetty/jetty.project/jetty-home/target/jetty-base/resources
+cp http.ini /Users/olamy/dev/sources/open-sources/jetty/jetty.project/jetty-home/target/jetty-base/start.d/
+
+cd /Users/olamy/dev/sources/open-sources/jetty/jetty.project/jetty-home/target/jetty-base
+/Library/Java/JavaVirtualMachines/jdk-11.0.2.jdk/Contents/Home/bin/java -Duser.language=en -Duser.country=US -jar ../jetty-home/start.jar 
